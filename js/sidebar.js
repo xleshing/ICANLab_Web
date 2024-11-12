97a410dfc0b448719f5a4ed09be72275b5de2916
Lue <xleshing21@gmail.com>
2024-11-13 02:15:33 +0800

    Update

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,8 @@
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     const toggle_button = document.querySelector("#toggle-button");
+    const open = toggle_button.querySelector("#open");
+    const close = toggle_button.querySelector("#close");
     const container = document.querySelector(".container");
 
     // 切換側邊欄的顯示狀態
@@ -9,17 +11,24 @@ function toggleSidebar() {
     // 切換按鈕的樣式類別
     toggle_button.classList.toggle("toggled");
 
-    // 切換按鈕圖示
+    // 檢查是否已經是 "toggled" 狀態
     if (toggle_button.classList.contains("toggled")) {
-        toggle_button.textContent = "☰"; // 側邊欄展開時顯示關閉圖示
-    } else {
-        toggle_button.textContent = "×"; // 側邊欄收起時顯示打開圖示
+        open.style.opacity = "0"; // 隱藏 "×"
+        setTimeout(function() {
+        close.style.opacity = "1"; // 顯示 "☰"
+        }, 250);
+    } else if (!toggle_button.classList.contains("toggled")) {
+        close.style.opacity = "0"; // 隱藏 "☰"
+        setTimeout(function() {
+        open.style.opacity = "1"; // 顯示 "×"
+        }, 250);
     }
 
-    // 調整 container 的 margin
+    // container隨著sidebar移動
     if (sidebar.classList.contains("collapsed")) {
         container.style.marginLeft = "0";
     } else {
         container.style.marginLeft = "250px";
     }
+
 }
