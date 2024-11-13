126637be3d817bc28df0c883a694c5247e0aa8ce
Lue <128021038+xleshing@users.noreply.github.com>
2024-11-13 12:42:30 +0800

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -9,8 +9,10 @@ function toggleSidebar() {
     sidebar.classList.toggle("collapsed");
 
     // 切換按鈕的樣式類別
-    toggle_button.classList.toggle("toggled");
-
+    setTimeout(function() {
+        toggle_button.classList.toggle("toggled");
+    }, 500);
+    
     // 檢查是否已經是 "toggled" 狀態
     if (toggle_button.classList.contains("toggled")) {
         setTimeout(function() {
